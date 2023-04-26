f9bd9021b712c4d511ec171dd7e3b00b710c29ca
ゆいち <victorkashima04@gmail.com>
2023-04-26 00:38:58 +0000

    FEAT: Split scripts validation

---
+++ scripts/login-form-validation.js
@@ -0,0 +1,63 @@
+const submitButton = document.querySelector('#submit');
+const errorMessage = document.getElementsByClassName("input-error");
+const allInput = document.getElementsByClassName("required");
+const loginForm = document.getElementById("loginForm");
+const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+const passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$/;
+const nameRegex = /^[a-záàâãéèêíïóôõöúçñ]+$/i;
+
+class User {
+    constructor(firstname, email, password) {
+      this.firstName = firstname;
+      this.email = email;
+      this.password = password
+    }
+  }
+
+loginForm.addEventListener("submit", function(e) {
+    
+    if (loginEmailValidate() && loginPasswordValidate()) {
+        signupForm.submit();
+    } else {
+        e.preventDefault();
+        loginEmailValidate();
+        loginPasswordValidate();
+    }
+
+})
+
+
+
+function loginEmailValidate() {
+
+    email = allInput[0].value;
+
+    if (emailRegex.test(email)) {
+        errorMessage[0].innerText = "";
+        return true;
+    } else if (email == "") {
+        errorMessage[0].innerText = "Preencha com seu e-mail";
+        return false;
+    } else {
+        errorMessage[0].innerText = "Digite o e-mail corretamente";
+        return false;
+    }
+
+}
+
+
+
+function loginPasswordValidate() {
+    password = allInput[1].value;
+
+    if (password.length >= 8) {
+        errorMessage[1].innerText = "";
+        return true;
+    } else if (password == "") {
+        errorMessage[1].innerText = "Preencha com sua senha";
+        return false;
+    } else {
+        errorMessage[1].innerText = "Sua senha deve conter no mínimo 8 caracteres"
+        return false;
+    }
+}
--- scripts/sign-form-validation.js
+++ scripts/sign-form-validation.js
@@ -2,7 +2,6 @@ const submitButton = document.querySelector('#submit');
 const errorMessage = document.getElementsByClassName("input-error");
 const allInput = document.getElementsByClassName("required");
 const signupForm = document.getElementById("signupForm");
-const loginForm = document.getElementById("loginForm");
 const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 const passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$/;
 const nameRegex = /^[a-záàâãéèêíïóôõöúçñ]+$/i;
@@ -19,7 +18,8 @@ signupForm.addEventListener("submit", function(e) {
     
     if (signNameValidate() && signEmailValidate() && signPasswordValidate() && signConfirmpasswordValidate()) {
         signupForm.submit();
-        //const user = new User();
+        const user = new User(nome, email, password);
+        console.log(user);
 
     } else {
         e.preventDefault();
@@ -106,56 +106,5 @@ function signConfirmpasswordValidate() {
     } else {
         errorMessage[3].innerText = "";
         return true;
-    }
-}
-
-
-//  LOGIN FORM VALIDATION
-
-loginForm.addEventListener("submit", function(e) {
-    
-    if (loginEmailValidate() && loginPasswordValidate()) {
-        signupForm.submit();
-    } else {
-        e.preventDefault();
-        loginEmailValidate();
-        loginPasswordValidate();
-    }
-
-})
-
-
-
-function loginEmailValidate() {
-
-    email = allInput[0].value;
-
-    if (emailRegex.test(email)) {
-        errorMessage[0].innerText = "";
-        return true;
-    } else if (email == "") {
-        errorMessage[0].innerText = "Preencha com seu e-mail";
-        return false;
-    } else {
-        errorMessage[0].innerText = "Digite o e-mail corretamente";
-        return false;
-    }
-
-}
-
-
-
-function loginPasswordValidate() {
-    password = allInput[1].value;
-
-    if (password.length >= 8) {
-        errorMessage[1].innerText = "";
-        return true;
-    } else if (password == "") {
-        errorMessage[1].innerText = "Preencha com sua senha";
-        return false;
-    } else {
-        errorMessage[1].innerText = "Sua senha deve conter no mínimo 8 caracteres"
-        return false;
     }
 }
